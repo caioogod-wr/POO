345c3594da8eaf0330f9b12a9205c902c69f156b
caioogod-wr <105987355+caioogod-wr@users.noreply.github.com>
2022-09-06 14:31:08 -0300

    Add files via upload

---
+++ Atv_1/1/processador.java
@@ -0,0 +1,22 @@
+package Atv_1;
+
+public class processador {
+    public int frequencia;
+    private int num_nucleos;
+
+    public void setFrequencia(int pFreq) {
+        frequencia = pFreq;
+    }
+
+    public int getFrequencia() {
+        return frequencia;
+    }
+
+    public void setNum_nucleos(int pNum) {
+        num_nucleos = pNum;
+    }
+
+    public int getNum_nucleos() {
+        return num_nucleos;
+    }
+}
