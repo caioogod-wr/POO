8307d90f2fc09208b5ac408fdfc3ec691b421ce5
caioogod-wr <105987355+caioogod-wr@users.noreply.github.com>
2022-09-06 14:27:34 -0300

    Add files via upload

---
+++ 1/tela_touch.java
@@ -0,0 +1,22 @@
+package Atv_1;
+
+public class tela_touch {
+    public boolean ligada;
+    private int brilho;
+
+    public void setLigada(boolean pLigada) {
+        ligada = pLigada;
+    }
+
+    public boolean getLigada() {
+        return ligada;
+    }
+
+    public void setBrilho(int pBrilho) {
+        brilho = pBrilho;
+    }
+
+    public int getBrilho() {
+        return brilho;
+    }
+}
