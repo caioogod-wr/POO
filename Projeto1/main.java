93efef27fdee19eb1d33df604b865f8b2d3f38b1
caioogod-wr <105987355+caioogod-wr@users.noreply.github.com>
2022-11-06 16:18:45 -0300

    Update main.java

--- Projeto1/main.java
+++ Projeto1/main.java
@@ -1,10 +1,17 @@
 package projeto1;
 
+
+/**
+ * @author caioo
+ * @version 1.0
+ * @since 1.0
+ */
 public class main {
     public static void main(String[] args) {
         Game game = new Game();
         
         System.out.println("Pressione enter para passar de tela ou \"q\" caso queira sair do jogo");
+        System.out.println("Nesta versão, os invasores são representados por \"!\" ");
         
         /**Inicia jogo*/
         game.run();
